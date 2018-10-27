b1d74f488ba7ab94efd07b2245e40a6c1dccecc9
Barczi Mihály <barczi.mihaly@gmail.com>
2018-10-27 17:50:35 +0200

    Added W02D1 Java Exercises 1-4

---
+++ week-02/day-1/src/IntroduceYourself.java
@@ -0,0 +1,17 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+        System.out.println("Hello. My name is Mihály Barczi.");
+        System.out.println("I am 26 years old.");
+        System.out.println("I am 1.72 meters tall.");
+    }
+}
