22e1a0eaf090a05666ecb05abfe0bb2569f32861
Barczi Mihály <barczi.mihaly@gmail.com>
2018-10-30 16:08:43 +0100

    Updated W02D2 Java Exercise 20

--- week-02/day-2/src/Unique.java
+++ week-02/day-2/src/Unique.java
@@ -12,7 +12,17 @@ public class Unique {
         int myArray[] = {1, 11, 34, 11, 52, 61, 1, 34};
         int uniqueArray[] = IntStream.of(myArray).distinct().toArray();
         for (int i = 0; i < Array.getLength(uniqueArray); i++) {
-            System.out.println(uniqueArray[i]);
+            System.out.print(uniqueArray[i] + " ");
+        }
+        System.out.println("");
+        uniqueMaker(myArray);
+
+    }
+
+    public static void uniqueMaker(int dasArray[]) {
+        int uniqueArray[] = IntStream.of(dasArray).distinct().toArray();
+        for (int i = 0; i < dasArray.length - 3; i++) { // 3, mert 3 duplicate szám van az Arrayben
+            System.out.print(uniqueArray[i] + " ");
         }
     }
 }
