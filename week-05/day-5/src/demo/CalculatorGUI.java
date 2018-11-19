2afb9c3673680c5c4895798c6aec0679f27d6a3c
Mihály Barczi <barczi.mihaly@gmail.com>
2018-11-19 08:40:38 +0100

    (CalculatorGUI): Add WindowListener methods

--- week-05/day-5/src/demo/CalculatorGUI.java
+++ week-05/day-5/src/demo/CalculatorGUI.java
@@ -3,7 +3,7 @@ package demo;
 import java.awt.*;
 import java.awt.event.*;
 
-public class CalculatorGUI extends Frame implements ActionListener {
+public class CalculatorGUI extends Frame implements ActionListener, WindowListener {
     Label textBeforeInputBox;
     Label textBeforeOutputSumBox;
     Label textBeforeOutputFactorialBox;
@@ -20,33 +20,34 @@ public class CalculatorGUI extends Frame implements ActionListener {
         this.textBeforeInputBox = new Label("Please enter a number: ");
         this.add(textBeforeInputBox);
 
-        this.inputInteger = new TextField(20);
+        this.inputInteger = new TextField(25);
         this.add(inputInteger);
         this.inputInteger.addActionListener(this);
 
         this.textBeforeOutputSumBox = new Label("The sum from 0 to the given number: ");
         this.add(textBeforeOutputSumBox);
 
-        this.outputResultSum = new TextField(20);
+        this.outputResultSum = new TextField(25);
         this.outputResultSum.setEditable(false);
         this.add(outputResultSum);
 
         this.textBeforeOutputFactorialBox = new Label("The factorial from 0 to the given number: ");
         this.add(textBeforeOutputFactorialBox);
 
-        this.outputResultFactorial = new TextField(20);
+        this.outputResultFactorial = new TextField(25);
         this.outputResultFactorial.setEditable(false);
         this.add(outputResultFactorial);
 
-        this.calculate = new Button("Calculate");
+        this.calculate = new Button("Calculate sum and factorial");
         this.add(calculate);
         this.calculate.addActionListener(this);
 
         this.setTitle("Hello TipTop");
-        this.setSize(245, 245);
+        this.setSize(300, 250);
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
+        addWindowListener(this);
     }
 
     public int factorialCalculator(int inputNumber) {
@@ -57,20 +58,40 @@ public class CalculatorGUI extends Frame implements ActionListener {
         }
     }
 
+    @Override
+    public void windowOpened(WindowEvent e) {
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+    }
+
+    @Override
     public void windowClosing(WindowEvent e) {
         dispose();
         System.exit(0);
     }
-    
-
-    public static void main(String[] args) {
-        CalculatorGUI mySimpleCalculator = new CalculatorGUI();
-    }
 
     @Override
-    public void actionPerformed(ActionEvent evt) {
+    public void actionPerformed(ActionEvent e) {
         int numberIn = Integer.parseInt(inputInteger.getText());
-        
+
         sum = 0;
         for (int i = 0; i <= numberIn; i++) {
             sum += i;
@@ -82,5 +103,9 @@ public class CalculatorGUI extends Frame implements ActionListener {
         inputInteger.setText("");
         outputResultSum.setText(sum + "");
         outputResultFactorial.setText(factorialSum + "");
+    }
+
+    public static void main(String[] args) {
+        CalculatorGUI mySimpleCalculator = new CalculatorGUI();
     }
 }
