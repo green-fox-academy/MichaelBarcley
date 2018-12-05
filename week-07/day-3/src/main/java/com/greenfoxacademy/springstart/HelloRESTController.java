89ca7d5dfb1f5142658242c3716235f04d96aa49
Mihály Barczi <barczi.mihaly@gmail.com>
2018-12-05 12:29:23 +0100

    W07D3 First Spring App: Add /greeting with user

--- week-07/day-3/src/main/java/com/greenfoxacademy/springstart/HelloRESTController.java
+++ week-07/day-3/src/main/java/com/greenfoxacademy/springstart/HelloRESTController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.springstart;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ public class HelloRESTController {
 
   @RequestMapping(value = "/greeting")
   @ResponseBody
-  public Greeting greeting() {
-    return new Greeting(1, "What's up mateys?!");
+  public Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "") String name) {
+    return new Greeting(1, "What's up " + name + "?");
   }
 }
