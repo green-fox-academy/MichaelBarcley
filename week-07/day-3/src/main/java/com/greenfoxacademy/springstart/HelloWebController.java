7d75dd38e59cd8bf9ce03d5a9d8a23841437e129
Mihály Barczi <barczi.mihaly@gmail.com>
2018-12-05 16:17:20 +0100

    W07D3 First Spring App: Refactor

--- week-07/day-3/src/main/java/com/greenfoxacademy/springstart/HelloWebController.java
+++ week-07/day-3/src/main/java/com/greenfoxacademy/springstart/HelloWebController.java
@@ -10,8 +10,6 @@ import java.util.concurrent.atomic.AtomicLong;
 @Controller
 public class HelloWebController {
   private AtomicLong id = new AtomicLong();
-  private String[] colors = {"red", "blue", "green"};
-  private int getColorId = (int) (Math.random() * colors.length);
   private String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
       "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
       "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
