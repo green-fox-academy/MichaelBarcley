f449d8e68c282d3230a4ef24ef73e574b65f3d0e
Mihály Barczi <barczi.mihaly@gmail.com>
2018-12-06 17:16:10 +0100

    W07D5 Webshop: Add base string return to webserver

--- week-07/day-5/src/main/java/com/greenfoxacademy/webshop/WebshopApplication.java
+++ week-07/day-5/src/main/java/com/greenfoxacademy/webshop/WebshopApplication.java
@@ -2,11 +2,22 @@ package com.greenfoxacademy.webshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class WebshopApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(WebshopApplication.class, args);
   }
+
+  @RequestMapping("/hello")
+  @ResponseBody
+  public String hello() {
+    return "Hello World!";
+  }
 }
+
