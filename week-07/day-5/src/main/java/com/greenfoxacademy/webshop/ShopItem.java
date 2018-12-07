a778e33181b92e30b3dea92413d3a6ee8468d614
Mihály Barczi <barczi.mihaly@gmail.com>
2018-12-07 11:16:45 +0100

    W07D5 Webshop: Items from backend are visible in html

--- week-07/day-5/src/main/java/com/greenfoxacademy/webshop/ShopItem.java
+++ week-07/day-5/src/main/java/com/greenfoxacademy/webshop/ShopItem.java
@@ -3,13 +3,29 @@ package com.greenfoxacademy.webshop;
 public class ShopItem {
   private String name;
   private String description;
-  private long price;
+  private double price;
   private int quantityOfStock;
 
-  public ShopItem(String name, String description, long price, int quantityOfStock) {
+  public ShopItem(String name, String description, double price, int quantityOfStock) {
     this.name = name;
     this.description = description;
     this.price = price;
     this.quantityOfStock = quantityOfStock;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public int getQuantityOfStock() {
+    return quantityOfStock;
   }
 }
--- week-07/day-5/src/main/java/com/greenfoxacademy/webshop/WebshopApplication.java
+++ week-07/day-5/src/main/java/com/greenfoxacademy/webshop/WebshopApplication.java
@@ -3,21 +3,35 @@ package com.greenfoxacademy.webshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @Controller
 public class WebshopApplication {
+  List<ShopItem> shopItemsList;
+
+  public WebshopApplication() {
+    shopItemsList = new ArrayList<>();
+    shopItemsList.add(new ShopItem("Running shoes", "Nike running shoes for every day sport", 1000.0, 5));
+    shopItemsList.add(new ShopItem("Printer", "Some HP printer that will print pages", 3000.0, 2));
+    shopItemsList.add(new ShopItem("Coca Cola", "0.5l standard coke", 25.0, 0));
+    shopItemsList.add(new ShopItem("Wokin", "Chicken with fried rice and WOKIN sauce", 119.0, 100));
+    shopItemsList.add(new ShopItem("T-shirt", "Blue with a corgi on a bike", 300.0, 1));
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(WebshopApplication.class, args);
   }
 
-  @RequestMapping("/hello")
-  @ResponseBody
-  public String hello() {
-    return "Hello World!";
+  @RequestMapping("/")
+  public String webshop(Model model) {
+    model.addAttribute("items", shopItemsList);
+    return "webshop";
   }
 }
 
