8e21c17ccde7ead7eb720d59b7f7e2c623c295c0
Mihály Barczi <barczi.mihaly@gmail.com>
2018-12-11 17:01:43 +0100

    W08D3 Thymeleaf: Add homework project

---
+++ week-08/day-3/Homework/src/main/java/com/greenfox/thymeleafhomework/ThymeleafhomeworkApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.thymeleafhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafhomeworkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThymeleafhomeworkApplication.class, args);
+  }
+}
