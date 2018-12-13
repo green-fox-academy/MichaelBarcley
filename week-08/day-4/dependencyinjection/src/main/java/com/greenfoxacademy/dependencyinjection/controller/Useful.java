7b8916528c50c8f8632aaacf86ca1fb01a07db43
Mihály Barczi <barczi.mihaly@gmail.com>
2018-12-13 14:50:00 +0100

    W08D4 Dependancy Injection Add email validation and Caesar encoder

--- week-08/day-4/dependencyinjection/src/main/java/com/greenfoxacademy/dependencyinjection/controller/Useful.java
+++ week-08/day-4/dependencyinjection/src/main/java/com/greenfoxacademy/dependencyinjection/controller/Useful.java
@@ -3,7 +3,9 @@ package com.greenfoxacademy.dependencyinjection.controller;
 import com.greenfoxacademy.dependencyinjection.service.UtilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Useful {
@@ -11,8 +13,35 @@ public class Useful {
   @Autowired
   UtilityService utilService;
 
-  @RequestMapping("/useful")
+  @GetMapping("/useful")
   public String usefulIndex() {
     return "useful";
+  }
+
+  @GetMapping("/useful/colored")
+  public String colored(Model model) {
+    model.addAttribute("color", utilService.randomColor());
+    return "colored";
+  }
+
+  @GetMapping("/useful/email")
+  public String isEmailValid (Model model,
+                              @RequestParam(value = "email", required = false, defaultValue = "example@example.com") String email) {
+    if (utilService.validateEmail(email)) {
+      model.addAttribute("color", "green");
+      model.addAttribute("text", email + " is a valid email address.");
+    } else {
+      model.addAttribute("color", "red");
+      model.addAttribute("text", email + " is not a valid email address.");
+    }
+    return "validemail";
+  }
+
+  @GetMapping("useful/caesar")
+  public String caesarCypher (Model model,
+                              @RequestParam(value = "text", required = false, defaultValue = "Caesar") String text,
+                              @RequestParam(value = "num", required = false, defaultValue = "1") int num) {
+    model.addAttribute("text", utilService.caesar(text, num));
+    return "caesar";
   }
 }
--- week-08/day-4/dependencyinjection/src/main/java/com/greenfoxacademy/dependencyinjection/service/UtilityService.java
+++ week-08/day-4/dependencyinjection/src/main/java/com/greenfoxacademy/dependencyinjection/service/UtilityService.java
@@ -23,4 +23,25 @@ public class UtilityService {
   public String randomColor() {
     return colors.get(random.nextInt(colors.size()));
   }
+
+  public boolean validateEmail(String email) {
+    if (email.contains("@") && email.contains(".")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public String caesar(String text, int number) {
+    if (number < 0) {
+      number = 26 + number;
+    }
+
+    String result = "";
+    for(int i = 0; i < text.length(); i++) {
+      int offset = Character.isUpperCase(text.charAt(i)) ? 'A' : 'a';
+      result += (char)(((int)text.charAt(i) + number - offset) % 26 + offset);
+    }
+    return result;
+  }
 }
