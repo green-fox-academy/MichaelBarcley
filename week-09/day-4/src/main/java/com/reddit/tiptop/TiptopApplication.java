b66157348ff7f851407210096ade91f3e6c2ada7
Mihály Barczi <barczi.mihaly@gmail.com>
2018-12-20 12:33:36 +0100

    W09D4 Reddit: Add static index.html with css

---
+++ week-09/day-4/src/main/java/com/reddit/tiptop/TiptopApplication.java
@@ -0,0 +1,14 @@
+package com.reddit.tiptop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiptopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TiptopApplication.class, args);
+  }
+
+}
+
