3ed00ab5b674682b97e1ccfc8ed746c8f0d29657
Mihály Barczi <barczi.mihaly@gmail.com>
2019-01-04 16:04:08 +0100

    W10D01 Rest: Add Frontend exercise

---
+++ week-10/day-1/src/main/java/com/gfa/w10d01/W10d01Application.java
@@ -0,0 +1,14 @@
+package com.gfa.w10d01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W10d01Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(W10d01Application.class, args);
+  }
+
+}
+
---
+++ week-10/day-1/src/test/java/com/gfa/w10d01/W10d01ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gfa.w10d01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class W10d01ApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
