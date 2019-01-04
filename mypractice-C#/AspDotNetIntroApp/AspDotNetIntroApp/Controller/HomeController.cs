c0e9da17c567c3fb84a45ad6cf7309552a2b2489
Mihály Barczi <barczi.mihaly@gmail.com>
2019-01-04 17:37:12 +0100

    C# Practice: Add basic MVC service to ASP.NET Intro App

---
+++ mypractice-C#/AspDotNetIntroApp/AspDotNetIntroApp/Controller/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotNetIntroApp.Controller
+{
+    public class HomeController
+    {
+        public String Index()
+        {
+            return "Hello from the Controller!";
+        }
+    }
+}
