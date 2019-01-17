ba51d398f9cab72866dbd8ba969552b9ba444bcb
Mihály Barczi <barczi.mihaly@gmail.com>
2019-01-17 15:31:36 +0100

    W11: Extend Movies webapp

--- week-11-projectstart/RazorMovies/RazorMovies/Migrations/20190117141701_Rating.Designer.cs
+++ week-11-projectstart/RazorMovies/RazorMovies/Migrations/20190117141701_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorMovies.Models;
 
 namespace RazorMovies.Migrations
 {
     [DbContext(typeof(RazorMoviesContext))]
-    partial class RazorMoviesContextModelSnapshot : ModelSnapshot
+    [Migration("20190117141701_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- week-11-projectstart/RazorMovies/RazorMovies/Models/Movie.cs
+++ week-11-projectstart/RazorMovies/RazorMovies/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +10,27 @@ namespace RazorMovies.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
+        [Display(Name = "Display Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 3)")]
         public decimal Price { get; set; }
+
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; }
     }
 }
