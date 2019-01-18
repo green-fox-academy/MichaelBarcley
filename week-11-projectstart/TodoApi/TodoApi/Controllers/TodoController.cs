3759833a24012019d8acdf6dd00df20579068c14
Mihály Barczi <barczi.mihaly@gmail.com>
2019-01-18 10:39:32 +0100

    W11: Add getAll and getTodoById to TodoApi app

---
+++ week-11-projectstart/TodoApi/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+using TodoApi.Repositories;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/todo")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext context;
+
+        public TodoController(TodoContext context)
+        {
+            this.context = context;
+
+            if (this.context.Todo.Count() < 2)
+            {
+                this.context.Todo.Add(new Todo { Name = "Buy milk" });
+                this.context.Todo.Add(new Todo { Name = "Go home", Description = "Tram 4-6, then M4", IsUrgent = true });
+                this.context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public List<Todo> GetAllTodoItems()
+        {
+            return context.Todo.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Todo GetTodoItem(long id)
+        {
+            return context.Todo.Find(id);
+        }
+    }
+}
---
+++ week-11-projectstart/TodoApi/TodoApi/Repositories/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Repositories
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Todo> Todo { get; set; }
+    }
+}
