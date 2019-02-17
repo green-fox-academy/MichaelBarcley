ff33bc8f41443b5c6666c85eff881fe6a318ce8b
Mihály Barczi <barczi.mihaly@gmail.com>
2019-02-17 17:15:25 +0100

    Add Ch5 of Tour Of Heroes AngularStart tutorial

--- JS-TS/AngularStart/src/app/heroes/heroes.component.ts
+++ JS-TS/AngularStart/src/app/heroes/heroes.component.ts
@@ -8,12 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
@@ -21,12 +16,8 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
-
 }
